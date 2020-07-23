52330bf08ffa665f707345131bcb7383d9c3ac1c
dawidmucha <dawidmucha98@gmail.com>
2020-07-23 22:14:32 +0200

    addClassroom list of subjects now updates in real time

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import Select from 'react-select'
 
@@ -11,23 +11,31 @@ const App = (props) => {
 		name: '',
 		abbv: ''
 	})
-	
+
+	let [subjects, setSubjects] = useState([])
+
+	useEffect(() => {
+		if(subjects.length !== props.subjects.length) handleStateSubjects()
+	})
+
 	const handleAddTeacher = (e) => {
 		e.preventDefault()
 
 		props.addTeacher(addTeacher(form.addTeacherName, form.addTeacherAbbv, 'pussy', 'cat doll'))
+		
+		handleStateSubjects()
 	}
 
 	const handleAddSubject = (e) => {
 		e.preventDefault()
-
+		
 		props.addSubject(addSubject(form.addSubjectName))
 	}
 
 	const handleAddClassroom = (e) => {
 		e.preventDefault()
 
-		props.addClassroom(addClassroom)
+		// props.addClassroom(addClassroom)
 	}
 
 	const handleChange = (e) => {
@@ -35,6 +43,15 @@ const App = (props) => {
 			...form,
 			[e.target.name]: e.target.value
 		})
+	}
+
+	const handleStateSubjects = () => {
+		setSubjects(Object.assign([], subjects, props.subjects.map(subject => {
+			return ({
+				label: subject.name,
+				value: subject.name
+			})
+		})))
 	}
 
 	return (
@@ -57,31 +74,35 @@ const App = (props) => {
 
 				<form onSubmit={(e) => handleAddClassroom(e)}>
 					<h1>ADD CLASSROOM</h1>
-					<Select options={[
-						{ label: 'i wanna', value: 'i wanna' },
-						{ label: 'kill myself', value: 'kill myself'}
-					 ]} />
+					<Select isMulti options={subjects} />
+					<button onClick={handleStateSubjects}>magic</button>
 				</form>
 			</div>
 
 			<div className='right'>
+				<h1>state</h1>
 				<div>
-						{JSON.stringify(form)}
+						form: {JSON.stringify(form)}
 				</div>
 				<div>
-					<h1>classes</h1>
+					 subjects: {JSON.stringify(subjects)}
+				</div>
+
+				<h1>props</h1>
+				<div>
+					<h2>classes</h2>
 					{JSON.stringify(props.classes)}
 				</div>
 				<div>
-					<h1>teachers</h1>
+					<h2>teachers</h2>
 					{JSON.stringify(props.teachers)}
 				</div>
 				<div>
-					<h1>classrooms</h1>
+					<h2>classrooms</h2>
 					{JSON.stringify(props.classrooms)}
 				</div>
 				<div>
-					<h1>subjects</h1>
+					<h2>subjects</h2>
 					{JSON.stringify(props.subjects)}
 				</div>
 			</div>
